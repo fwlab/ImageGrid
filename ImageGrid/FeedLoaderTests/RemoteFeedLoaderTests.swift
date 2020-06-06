259e9faa1b3eccfdd222878629bed8b5246e7b41
Michele Fadda <mgf64mba@gmail.com>
2020-06-06 02:15:11 +0100

    refactoring tests
    Signed-off-by: Michele Fadda <mgf64mba@gmail.com>

--- ImageGrid/FeedLoaderTests/RemoteFeedLoaderTests.swift
+++ ImageGrid/FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -21,15 +21,6 @@ protocol HTTPClient {
     func get(from url: URL, completion: (Results)-> Void )
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURLs = [URL]()
-
-    func get(from url: URL, completion: (Results) -> Void) {
-        requestedURLs.append(url)
-    }
-    
-
-}
 
 
 class RemoteFeedLoader: FeedLoader {
@@ -51,23 +42,20 @@ class RemoteFeedLoader: FeedLoader {
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataUponCreation() {
-        let client = HTTPClientSpy()
-        _ = makeSUT(client: client)
+        let (_,client) = makeSUT()
         XCTAssertEqual(client.requestedURLs.count,0)
     }
 
     func test_load_RequestsDataFromURL() {
         let url = anyURL()
-        let client = HTTPClientSpy()
-        let sut = makeSUT(url: url, client:client)
+        let (sut,client) = makeSUT(url: url)
         sut.load{ _ in }
         XCTAssertEqual(client.requestedURLs,[url])
     }
     
     func test_loadTwice_RequestsDataFromURLTwice() {
         let url = anyURL()
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(from: url, client:client)
+        let (sut,client) = makeSUT(url: url)
         sut.load{ _ in }
         sut.load{ _ in }
         XCTAssertEqual(client.requestedURLs,[url,url])
@@ -78,14 +66,21 @@ class RemoteFeedLoaderTests: XCTestCase {
     
  // MARK Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://any-url.com")!, client: HTTPClient ) -> RemoteFeedLoader {
-        return RemoteFeedLoader(from: url, client: client)
+    class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+
+        func get(from url: URL, completion: (Results) -> Void) {
+            requestedURLs.append(url)
+        }
     }
+
+    
+    private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        return (RemoteFeedLoader(from: url, client: client), client)
+    }
+    
     func anyURL() -> URL {
         return URL(string: "https://any-url.com")!
     }
-    
-
-
-
 }
